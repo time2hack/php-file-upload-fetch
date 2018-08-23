9936ba02e43c2f2864c9ca1f439bd7a27db68ee8
Pankaj Patel <p.patel@holidaypirates.com>
2018-08-23 12:57:20 +0200

    add file manually

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const button = document.querySelector('#submit');
+const submitButton = document.querySelector('#submit');
+const uploadButton = document.querySelector('#upload');
 
 
-button.addEventListener('click', () => {
+submitButton.addEventListener('click', () => {
   const form = new FormData(document.querySelector('#profileData'));
   const headers = new Headers();
+  const url = './upload.php'
+  const request = new Request(url, {
+    method: 'POST',
+    headers: headers,
+    body: form
+  });
+
+  fetch(request)
+    .then(response => response.json())
+    .then(data => { console.log(data); })
+})
+
+uploadButton.addEventListener('click', () => {
+  const form = new FormData();
+  const file = document.querySelector('#upfile-2').files[0];
+  form.append('upfile', file);
+  const headers = new Headers();
+  
   const url = './upload.php'
   const request = new Request(url, {
     method: 'POST',
